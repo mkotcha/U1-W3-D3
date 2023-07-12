108c77435aa301b958c6b3204693f3c88a8ba7d6
mkotcha <matteo.falasca@emmek.org>
2023-07-12 15:28:41 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const addTask = addEvent => {
+  addEvent.preventDefault();
+  const element = addEvent.target.elements[0];
+  document.querySelector("main > section + section").innerHTML += `<div>
+            <i class="fa-regular fa-square"></i>
+            <i class="fa-solid fa-check"></i>
+            ${element.value}
+            <i class="fa-solid fa-trash-can"></i>
+        </div>`;
+  document.querySelector("section > div:last-child").addEventListener("click", doneTask);
+  document.querySelector("div > i:last-child").addEventListener("click", delTask);
+};
+
+const doneTask = doneEvent => doneEvent.currentTarget.classList.toggle("done");
+
+const delTask = delEvent => delEvent.target.parentElement.remove();
