664de5b1910e61ac0da76abc575f24b738379fe5
mkotcha <matteo.falasca@emmek.org>
2023-07-12 15:48:44 +0200

    some fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 const addTask = addEvent => {
   addEvent.preventDefault();
   const element = addEvent.target.elements[0];
-  document.querySelector("main > section + section").innerHTML += `<div>
-            <i class="fa-regular fa-square"></i>
-            <i class="fa-solid fa-check"></i>
-            ${element.value}
-            <i class="fa-solid fa-trash-can"></i>
+  document.querySelector("main > section + section").innerHTML += `<div onclick="doneTask(event)">
+          <i class="fa-regular fa-square"></i><i class="fa-solid fa-check"></i>${element.value}<i
+            class="fa-solid fa-trash-can"
+            onclick="delTask(event)"></i>
         </div>`;
-  document.querySelector("section > div:last-child").addEventListener("click", doneTask);
-  document.querySelector("div > i:last-child").addEventListener("click", delTask);
+
+  // document.querySelector("section > div:last-child").addEventListener("click", doneTask);
+  // document.querySelector("div > i:last-child").addEventListener("click", delTask);
 };
 
 const doneTask = doneEvent => doneEvent.currentTarget.classList.toggle("done");
